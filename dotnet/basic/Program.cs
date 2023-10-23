4f78388f09b0d9d966ab4a5adaa57271dd704472
Gabriel Fernella <gabrielfernella07@hotmail.com>
2023-10-23 02:41:59 -0300

    basic class dotnet

---
+++ dotnet/basic/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using dotnet.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 51.45M);
+
+c1.Sacar(65);
+
+c1.ExibirSaldo();
+
+
+
+/*Pessoa p1 = new Pessoa();
+
+p1.Nome = "Fernella";
+p1.Idade = 26;
+
+p1.Apresentar();*/
